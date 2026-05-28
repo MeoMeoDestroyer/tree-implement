1265acd1bd647f4968763930d5f485d71be60c3f
Kate_Nguyen <Kate_nguyen>
2026-05-28 11:44:01 -0700

    in class

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,26 @@
 public class TreeNode {
+public int data;
+public TreeNode left;  
+public TreeNode right;
 
-  
+public TreeNode (int data){
+    this.data = data;
 }
+
+public TreeNode (int data, TreeNode left){
+    this.data = data;
+    this.left = left;
+}
+
+public TreeNode (int data, TreeNode right){
+    this.data = data;
+    this.right = right;
+}
+
+public TreeNode (int data, TreeNode left, TreeNode right) {
+    this.data = data;
+    this.right = right;
+    this.left = left;
+}
+}
+
