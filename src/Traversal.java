75e9023f807cede843c4a385d907e71bf82516a1
Kate_Nguyen <Kate_nguyen>
2026-06-02 11:40:41 -0700

    in class

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,19 +1,56 @@
 public class Traversal {
   public static void main(String[] args) {
-    TreeNode root = new TreeNode(10);
-    root.left = new TreeNode(9);
-    root.left.left = new TreeNode(5);
-    root.left.right = new TreeNode(2);
+    TreeNode<Integer> root = new TreeNode<>(10);
+    root.left = new TreeNode<>(9);
+    root.left.left = new TreeNode<>(5);
+    root.left.right = new TreeNode<>(2);
 
-    root.right = new TreeNode(15);
-    root.right.left = new TreeNode(-3);
-    root.right.right = new TreeNode(5);
-    root.right.right.right = new TreeNode(22);
+    root.right = new TreeNode<>(15);
+    root.right.left = new TreeNode<>(-3);
+    root.right.right = new TreeNode<>(5);
+    root.right.right.right = new TreeNode<>(22);
+
+
+        TreeNode<String> stringRoot = new TreeNode<>("hello");
+    stringRoot.left = new TreeNode<>("hi");
+    stringRoot.left.left = new TreeNode<>("yay");
+    stringRoot.left.right = new TreeNode<>("hungry");
+
+stringRoot.right = new TreeNode<>("ok",
+                       new TreeNode<>("huh"),
+                       new TreeNode<>("eat",
+                       null,
+                           new TreeNode<>("nhom")
+                       )
+                          );
    // preorder(root);
-   postOrder(root);
+   //postOrder(StringRoot);
+   //printGreaterThan(root,1 );
+   System.out.println(countNodes(root));
   }
 
-  public static void preorder(TreeNode current){
+  public static int countNodes(TreeNode<?> current){
+    //if (current == null) return 0;
+      // int leftCount = countNodes(current.left);
+      // int rightCount = countNodes(current.right);
+      //int totalCount = 1 + leftCount + rightCount;
+
+    //bool?iftrue:else;
+    // short 1 line
+    // return current == null ? 0  : 1 + countNodes(current.left) + countNodes(current.right);
+    return 1 + countNodes(current.left) + countNodes(current.right);
+  }
+
+  public static void printGreaterThan(TreeNode<Integer> current, int threshold) {
+    if (current == null) return;
+    if (current.data > threshold) {
+      System.out.println(current.data);
+      printGreaterThan(current.left, threshold);
+      printGreaterThan(current.right, threshold);
+    }
+  }
+
+  public static void preorder(TreeNode<?> current){
     if (current == null) return;
     System.out.println(current.data);
 
@@ -21,14 +58,15 @@ public class Traversal {
     preorder(current.right);
   }
 
-  public static void postOrder(TreeNode current){
+  public static <E> void postOrder(TreeNode<E> current){
     if (current == null) return;
     postOrder(current.left);
     postOrder(current.right);
+    E myData = current.data;
     System.out.println(current.data);
   }
 
-  public static void inOrder(TreeNode current){
+  public static void inOrder(TreeNode<?> current){
     if (current == null) return;
     inOrder(current.left);
     System.out.println(current.data);
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,23 +1,23 @@
-public class TreeNode {
-public int data;
-public TreeNode left;  
-public TreeNode right;
+public class TreeNode <T> {
+public T data;
+public TreeNode<T> left;  
+public TreeNode<T> right;
 
-public TreeNode (int data){
+public TreeNode (T data){
     this.data = data;
 }
 
-public TreeNode (int data, TreeNode left){
+public TreeNode (T data, TreeNode<T> left){
     this.data = data;
     this.left = left;
 }
 
-public TreeNode (int data, TreeNode right){
+public TreeNode (T data, TreeNode<T> right){
     this.data = data;
     this.right = right;
 }
-
-public TreeNode (int data, TreeNode left, TreeNode right) {
+//TreeNode<Interger> myNode = new TreeNode<>(8, null, someOtherNode);
+public TreeNode (T data, TreeNode<T> left, TreeNode<T> right) {
     this.data = data;
     this.right = right;
     this.left = left;
